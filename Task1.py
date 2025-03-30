d25a585574a61957729403f79d6b6d309e7153a6
viswa159 <53033350+viswa159@users.noreply.github.com>
2025-03-30 17:55:00 +0530

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+sum = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+print('Addition: ' + str(sum))
+print('Subtraction: ' + str(sub))
+print('Multiplication: ' + str(mul))
+print('Division: ' + str(div))
